9799eec5236673eec4224a45f13e75e1ed01edce
oddstap <matthewrafei1@gmail.com>
2024-04-21 17:50:20 -0500

    Fixed over complicated enum.c code

--- Section_5_Challenge_1/enum.c
+++ Section_5_Challenge_1/enum.c
@@ -10,40 +10,15 @@ enum Company{
 	Microsoft
 };
 
-
-int get_index(enum Company testcase)
-{
-	switch(testcase){
-		case Google :
-			return 0;
-			break;
-		case Facebook :
-			return 1;
-			break;
-		case Xerox :
-			return 2;
-			break;
-		case Yahoo :
-			return 3;
-			break;
-		case Ebay :
-			return 4;
-			break;
-		case Microsoft :
-			return 5;
-			break;
-	}
-}
-
 int main()
 {
 	enum Company ebay = Ebay;
 	enum Company xerox = Xerox;
 	enum Company google = Google;
 
-	printf("%d\n", get_index(xerox));
-	printf("%d\n", get_index(google));
-	printf("%d\n", get_index(ebay));
+	printf("%d\n", xerox);
+	printf("%d\n", google);
+	printf("%d\n", ebay);
 
 
 
