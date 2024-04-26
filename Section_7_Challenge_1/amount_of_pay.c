6bd4d1c9be5247b20a17232afa0f6e38417d0900
MatthewRafei <matthewrafei1@gmail.com>
2024-04-25 23:39:08 -0500

    Too tired to program effectively. Will finish Section_7 challenge later

--- Section_7_Challenge_1/amount_of_pay.c
+++ Section_7_Challenge_1/amount_of_pay.c
@@ -4,15 +4,36 @@
 int main()
 {
 
-	float pay_rate = 12.0;
+	const float pay_rate = 12.0;
 	float hours = 0;
 
 	printf("How many hours have your worked this week: ");
 	scanf("%f", &hours);
 
-	if(hours > 40){
-		float overtime = 0;
-		float reg_pay = 0;
+	if(hours > 40.0){
+		float overtime_hours = 0.0;
+		float overtime_pay = 0.0;
+
+		float reg_pay = 40.0 * pay_rate;
+
+		overtime_hours = hours - 40.0;
+		overtime_pay = (overtime_hours * pay_rate) * 1.5;
+
+		float total_pay = reg_pay + overtime_pay;
+	}
+	else{
+		float total_pay = hours * pay_rate;
+	}
+
+	if(total_pay > 300){
+		float fifteen_per_tax_income = 0;
+	}
+
+
+
+
+
+		
 
 
 
