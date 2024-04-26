5e85052ad54236b9639fed0096f80d7e00bb7020
oddstap <matthewrafei1@gmail.com>
2024-04-25 21:32:48 -0500

    Working on section 7 now

---
+++ Section_7_Challenge_1/amount_of_pay.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+
+	float pay_rate = 12.0;
+	float hours = 0;
+
+	printf("How many hours have your worked this week: ");
+	scanf("%f", &hours);
+
+	if(hours > 40){
+		float overtime = 0;
+		float reg_pay = 0;
+
+
+
+
+
+
+
+	return 0;
+}
